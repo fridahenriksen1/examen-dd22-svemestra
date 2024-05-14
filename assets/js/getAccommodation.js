26cbf50f4f5276de604390eb13f3a67cd4cc94c8
Frida Henriksen <fridahenriksen@hotmail.com>
2024-05-14 12:33:02 +0200

    Hjälp och fix av Lissel

--- assets/js/getAccommodation.js
+++ assets/js/getAccommodation.js
@@ -28,6 +28,41 @@ fetch('../json/accommodation.json')
             //Gör en ny variabel  för varje html som ska ha data
             const img = destination.querySelector('[data-img]');
             img.setAttribute('src', `../${item.imageURL}`);
+            const slider = destination.querySelector(".reviews-box");
+            let isDown = false;
+            let startX;
+            let scrollLeft;
+            console.log(slider);
+        
+          
+            if (slider) {
+              slider.addEventListener("mousedown", (e) => {
+                isDown = true;
+                slider.classList.add("active");
+                startX = e.pageX - slider.offsetLeft;
+                scrollLeft = slider.scrollLeft;
+              });
+          
+              slider.addEventListener("mouseleave", () => {
+                isDown = false;
+                slider.classList.remove("active");
+              });
+          
+              slider.addEventListener("mouseup", () => {
+                isDown = false;
+                slider.classList.remove("active");
+              });
+          
+              slider.addEventListener("mousemove", (e) => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - slider.offsetLeft;
+                const walk = (x - startX) * 2; // Adjust scroll speed here
+                slider.scrollLeft = scrollLeft - walk;
+              });
+            } else {
+              console.error("Element with class 'reviews-box' not found");
+            }
             
             //Lägger vi till det vi skapat i .accomandation div
             container.append(destination);
